1d046ad4c56845ce88d495aa1fae4f70c5f930db
alvaromago <aalvaromago@gmail.com>
2023-03-15 22:04:11 +0100

    "First commit"

---
+++ holamundo.py
@@ -0,0 +1,8 @@
+# Esto es un comentario
+"""
+Este es un
+comentario en
+varias líneas
+"""
+# Hola Mundo
+print("Hola Mundo")
---
+++ variables.py
@@ -0,0 +1,30 @@
+"""
+# Los nombres de las variables solo admiten minúsculas y _
+# No admiten -, @, $, ni empezar por números
+"""
+variable_string = "Variable String"
+print(variable_string)
+
+variable_int = 5
+print(variable_int)
+
+variable_float = 2.5
+print(variable_float)
+
+variable_boolean = True
+print(variable_boolean)
+
+# Convertir int to String
+variable_int_to_string = str(variable_int)
+
+# Concatenación de variables en un print
+print(variable_string, variable_int, variable_boolean)
+print("El valor de mi variable String es:", variable_string, ". El valor de mi variable int es:", variable_int)
+
+# Función .length
+print(len(variable_string))
+
+# Ejemplo input
+nombre = input("¿Cómo te llamas? ")
+edad = input("¿Cuántos años tienes? ")
+print(nombre, edad)
