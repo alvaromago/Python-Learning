86ac852e328fd734f02a3b4a304b956828ffc721
alvaromago <aalvaromago@gmail.com>
2023-03-16 00:33:40 +0100

    Ejercicios Python

---
+++ T1/par_impar.py
@@ -0,0 +1,7 @@
+num = int(input("Dime un número: "))
+if(num%2==0):{
+    print("El número es PAR")
+}
+else:{
+    print("El número es IMPAR")
+}
---
+++ T1/suma.py
@@ -0,0 +1,3 @@
+num1 = int(input("Introduce un número: "))
+num2 = int(input("Introduce otro número: "))
+print("La suma de los dos números es: " + str(num1 + num2))
