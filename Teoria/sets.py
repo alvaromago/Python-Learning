ed259433f99493a070f543f06561b3917cdb9b20
alvaromago <aalvaromago@gmail.com>
2023-03-19 22:06:39 +0100

    Teoría Python

---
+++ Teoria/sets.py
@@ -0,0 +1,6 @@
+set1 = set()
+set2 = {}
+
+print(type(set2)) # type = dict
+set2 = {"Álvaro", "Martín", 21}
+print(type(set2)) # type = set
--- Teoria/tuplas.py
+++ Teoria/tuplas.py
@@ -8,5 +8,8 @@ tuple1 = (21, 1.80, "Álvaro", "Martín")
 print(tuple1) # type = tuple
 print(tuple1[0])
 print(tuple1[-2]) # Empezando por atrás
-
 print(tuple1.index("Martín")) # Índice que tiene ese contenido
+
+tuple2 = (21, 30, 24, 23, 24, 28)
+print(tuple1 + tuple2) # Se pueden concatenar tuplas
+
