e5286ed59fd2c91ce051a3f4ef4c2a07c0c162ba
alvaromago <aalvaromago@gmail.com>
2023-03-30 20:30:23 +0200

    Teoria Diccionarios

--- Teoria/diccionarios.py
+++ Teoria/diccionarios.py
@@ -21,3 +21,10 @@ print(dict1)
 
 print("Álvaro" in dict1) # false
 print("Nombre" in dict1) # true (busca por clave no por valor)
+
+print(dict1.items()) # Muestra las claves y valores unidos en formato lista
+print(dict1.keys()) # Muestra las claves en formato lista
+print(dict1.values()) # Muestra los valores en formato lista
+
+dict3 = dict.fromkeys(("Nombre", "Edad")) # Esta función crea un diccionario vacio con las claves que tu le introduzcas
+print(dict3)
