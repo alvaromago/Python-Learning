a27982a9355015ea4d9dbcab5280323219a1b202
alvaromago <aalvaromago@gmail.com>
2023-04-13 21:40:21 +0200

    Teoria Funciones

--- Teoria/funciones.py
+++ Teoria/funciones.py
@@ -9,3 +9,25 @@ def suma(num1, num2):
     print(num1 + num2)
 
 suma(5,5)
+
+def suma_con_return(num1, num2):
+    return num1 + num2
+
+resultado = suma_con_return(3,6)
+print(resultado)
+
+def print_nombre(nombre, apellido):
+    print(f"{nombre} {apellido}")
+
+print_nombre(apellido = "Martin", nombre = "Alvaro") # Puedes reordenar el orden de los valores
+
+def print_nombre_con_default(nombre, apellido, alias = "Sin alias"): # Puedes definir valores por defecto para un parámetro
+    print(f"{nombre} {apellido}, {alias}")
+
+print_nombre_con_default("Alvaro", "Martin")
+
+def print_textos_mayusculas(*textos): # Con el asterisco defines que pueden pasarle de 1 a infintos valores a esa función
+    for texto in textos:
+        print(texto.upper())
+
+print_textos_mayusculas("Hola", "Prueba de Python", "Parámetros infinitos")
