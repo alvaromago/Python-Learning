40a06613aba8422acfda6e286e7dc482b35d5b27
alvaromago <aalvaromago@gmail.com>
2023-04-28 19:04:48 +0200

    Teoría módulos

---
+++ Teoria/modulos.py
@@ -0,0 +1,14 @@
+import modulo
+from modulo import resta, escribir # Puedo importar una función en concreto
+
+modulo.resta(5, 3)
+modulo.escribir("Hola")
+
+import math
+
+print(math.pi)
+print(math.pow(2, 3)) # 2 elevado a 3
+
+from math import pi as VALOR_PI
+
+print(VALOR_PI)
