95ed9d7c4a676ec9e276e9e9ab1318f88c389747
alvaromago <aalvaromago@gmail.com>
2023-04-30 03:08:09 +0200

    Tkinter Basics

--- T4/tkinterBasics.py
+++ T4/tkinterBasics.py
@@ -1,8 +1,24 @@
 import tkinter
 
-ventana = tkinter.Tk()
-ventana.geometry("500x400")
-etiqueta = tkinter.Label(ventana, text="Hola Mundo")
-etiqueta.pack()
+ventana = tkinter.Tk()  # Definición de una ventana
+ventana.geometry("500x400")  # Tamaño
+
+## Label ##
+lbl1 = tkinter.Label(ventana, text="Hola Mundo")
+lbl1.pack()  # Poner 'etiqueta' en la pantalla
+lbl2 = tkinter.Label(ventana, text="Texto inferior", bg="grey")
+lbl2.pack(side=tkinter.BOTTOM)
+
+
+def saludo():
+    print("Hola")
+
+
+## Button ##
+# La llamada a la función debe ir sin paréntesis
+btn1 = tkinter.Button(ventana, text="Pulsa", command=saludo)
+btn1.pack()
+
+## TextField ##
 
 ventana.mainloop()
