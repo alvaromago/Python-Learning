263ebf70b40380ba8de6644b45b4f9d95c4e4457
alvaromago <aalvaromago@gmail.com>
2023-05-12 20:12:12 +0200

    dates

--- Teoria Intermedia/dates.py
+++ Teoria Intermedia/dates.py
@@ -1,9 +1,27 @@
+from datetime import date
+from datetime import time
 import datetime
 
-fecha_actual = datetime.datetime.now()
-print("Hora actual:", fecha_actual.hour)
-print("Mes actual", fecha_actual.month)
-print("Día actual:", fecha_actual.day)
+momento_actual = datetime.datetime.now()
+print("Hora actual:", momento_actual.hour)
+print("Mes actual", momento_actual.month)
+print("Día actual:", momento_actual.day)
 
-timestamp = fecha_actual.timestamp()
+timestamp = momento_actual.timestamp()
 print(timestamp)
+
+
+tiempo_actual = time(19, 30, 0)
+print(tiempo_actual.hour)
+print(tiempo_actual.minute)
+print(tiempo_actual.second)
+
+fecha_actual = date.today()  # Actual
+print(fecha_actual.year)
+print(fecha_actual.month)
+print(fecha_actual.day)
+
+fecha_actual = date(2023, 8, 4)  # Definirlo tú
+print(fecha_actual.year)
+print(fecha_actual.month)
+print(fecha_actual.day)
